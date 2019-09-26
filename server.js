9aba83c1ad34c203db1fa1875a9ed73dadd14e9f
Hamid Taheri <h.taheri@sadad.co.ir>
2019-09-26 14:04:53 +0330

    change service

--- server.js
+++ server.js
@@ -1,107 +1,42 @@
-var app 	   = require('express')();
-var url 	   = require('url');
-var bodyParser = require('body-parser');
-var rq 		   = require('request');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
 
- app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Content-Type', 'application/json');
-  res.header('Accept', 'application/json');
-  next();
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
+
+app.use(bodyParser.json({limit: myLimit}));
+
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL =  'http://192.168.0.1:8081' // YOUR_URL;;
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
 });
 
-// YOUR_REST_API_IP_ADDRESS 
-var proxy_url = "0.0.0.0";
+app.set('port', 9080); // Service Port
 
-
-app.get('/*', function(req, res, next){
-	var _url = url.parse(req.url);
-	var url_ = "";
-		console.log();
-		if(_url.query == null ){
-			url_ = 'https://'+proxy_url+_url.pathname;
-			console.log('GET : '+url_);
-		}else {
-			url_ = 'https://'+proxy_url+_url.pathname+'?'+_url.query;
-			console.log('GET : '+url_);
-			
-		}
-	rq(url_, function (err, response, body) {
-		if(!err && response.statusCode == 200) {
-			var obj = JSON.parse(body);
-			res.json(obj);
-
-		}else
-			res.json(err);
-	});
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
 });
-
-app.put('/*', function(req, res, next){
-	var _url = url.parse(req.url).pathname;
-	console.log('PUT : https://'+proxy_url+_url);
-	rq({ headers: [{'Content-Type': ' application/json',
-					    'Accept'      : 'application/json'}],
-			  url    :'https://'+proxy_url+_url,
-			  method : 'PUT',
-			  json   : req.body
-			}, function (err, response, body) {
-		if(!err && response.statusCode == 200) {
-			res.json(body);
-
-		}else
-			res.json(err);
-	});
-});
-
-
-
-app.delete('/*', function(req, res, next){
-	var _url = url.parse(req.url).pathname;
-	console.log('DELETE : https://'+proxy_url+_url);
-	rq({ headers: [{'Content-Type': ' application/json',
-					    'Accept'      : 'application/json'}],
-			  url    :'https://'+proxy_url+_url,
-			  method : 'DELETE'
-			}, function (err, response, body) {
-		if(!err && response.statusCode == 200) {
-			res.json(body);
-		}else
-			res.json(err);
-	});
-});
-
-app.post('/*', function(req, res, next){
-	var _url = url.parse(req.url).pathname;
-	console.log('POST : https://'+proxy_url+_url);
-	console.log(req.body);
-	rq({ headers: [{'Content-Type': ' application/json',
-					    'Accept'      : 'application/json'}],
-			  url    :'https://'+proxy_url+_url,
-			  method : 'POST',
-			  json   : req.body
-			}, function (err, response, body) {
-		if(!err && response.statusCode == 200) {
-			//var obj = JSON.parse(body);
-			res.json(body);
-
-		}else
-			res.json(err);
-	});
-});
-
-
-
-app.listen(8000, function (){	
-console.log('welcome to `CPROXY_URL` API Proxy server');
-console.log('viewed at : http://localhost:8000');
-});
-
- 
-
-
